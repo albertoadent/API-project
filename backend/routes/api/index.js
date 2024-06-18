f992f552b10bca7ab84958f4d30c86a581778a5e
Alberto <albertoadent@gmail.com>
2024-06-18 12:44:21 -0400

    phase3-1

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,57 @@
 // backend/routes/api/index.js
 const router = require("express").Router();
 
+const { restoreUser, requireAuth } = require("../../utils/auth.js");
+
+/*
+
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
 });
+
+// GET /api/set-token-cookie
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+} = require("../../utils/auth.js");
+
+const { User } = require("../../db/models");
+router.get("/set-token-cookie", async (_req, res) => {
+  const user = await User.findOne({
+    where: {
+      username: "Demo-lition",
+    },
+  });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
+// GET /api/restore-user
+
+router.use(restoreUser);
+
+router.get("/restore-user", (req, res) => {
+  return res.json(req.user);
+});
+
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+*/
+
+// restore the surrect user session to req.user
+router.use(restoreUser);
+
+router.use([() => null]); //all routes that don't require authentication
+
+router.use(requireAuth);
+
+router.use([() => null]); //all routes that require authentication
 
 module.exports = router;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-// backend/routes/index.js
-// ...
+
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -12,13 +11,9 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
-// ...
 
-// backend/routes/index.js
-// ...
 const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
-// ...
 
 module.exports = router;
