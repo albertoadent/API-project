42189b4e1e27911df4df4d1b7435d316154aee05
Alberto <albertoadent@gmail.com>
2024-06-19 14:23:00 -0400

    add user input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -4,14 +4,33 @@ const bcrypt = require("bcryptjs");
 
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
+
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const validateLogin = [(req,res,next)=>{
+    const {username, email} = req.body
+    req.body.credential = username ? username : email;
+    return next();
+},
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
 const router = express.Router();
 
-router.post("/", async (req, res, next) => {
+router.post("/", validateLogin, async (req, res, next) => {
   const { username, password, email } = req.body;
   try {
     if (!((username || email) && password))
       throw new Error("insufficient credentials given");
-    const credential = username ? username : email;
+    const {credential} = req.body;
     const user = await User.unscoped().findOne({
       where: {
         [Op.or]: { username: credential, email: credential },
@@ -51,7 +70,7 @@ router.delete("/", (_req, res) => {
 });
 
 router.get("/", (req, res) => {
-  const {user} = req;
+  const { user } = req;
   if (user) {
     const safeUser = {
       id: user.id,
