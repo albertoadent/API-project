0686d1b66acedda403aece583c1adbb4f7de1a12
Alberto <albertoadent@gmail.com>
2024-06-20 02:58:27 -0400

    edited email max length

--- backend/db/migrations/20240618003938-create-user.js
+++ backend/db/migrations/20240618003938-create-user.js
@@ -31,7 +31,7 @@ module.exports = {
           unique: true,
         },
         email: {
-          type: Sequelize.STRING(256),
+          type: Sequelize.STRING(255),
           allowNull: false,
           unique: true,
         },
