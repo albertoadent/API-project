c1295f5130fddd08841091090cc11e88e6511fef
Alberto <albertoadent@gmail.com>
2024-06-20 23:09:27 -0400

    fixed

--- backend/db/migrations/20240621015854-create-event-member.js
+++ backend/db/migrations/20240621015854-create-event-member.js
@@ -9,34 +9,38 @@ if (process.env.NODE_ENV === "production") {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("Event_Members", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
+    await queryInterface.createTable(
+      "Event_Members",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        groupMemberId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: "Group_Members" },
+        },
+        eventId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: { model: "Events" },
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
       },
-      groupMemberId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: { model: "Group_Members" },
-      },
-      eventId: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        references: { model: "Events" },
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
-      },
-    });
+      options
+    );
   },
   async down(queryInterface, Sequelize) {
     options.tableName = "Event_Members";
