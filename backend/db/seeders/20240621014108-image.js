10b5a6e0b8dec9509645a1c33b4432d8878811f5
Alberto <albertoadent@gmail.com>
2024-06-20 21:46:47 -0400

    image model,migration, and seeder done

---
+++ backend/db/seeders/20240621014108-image.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const { Image } = require("../models");
+
+const options = {};
+
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await Image.bulkCreate(
+      [
+        {
+          url: "imageUrl1",
+        },
+        { url: "imageUrl2" },
+      ],
+      { validate: true }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Images";
+    await queryInterface.bulkDelete(
+      options,
+      {
+        url: { [Sequelize.Op.in]: ["imageUrl1", "imageUrl2"] },
+      },
+      {}
+    );
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
